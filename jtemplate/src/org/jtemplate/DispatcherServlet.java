843d75d647b86b939853250a7456370fe8cf5284
Greg Brown <gk_brown@verizon.net>
2016-10-14 17:06:10 -0400

    Begin stubbing out request handling logic.

--- jtemplate/src/org/jtemplate/DispatcherServlet.java
+++ jtemplate/src/org/jtemplate/DispatcherServlet.java
@@ -14,20 +14,373 @@
 
 package org.jtemplate;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  * Abstract base class for dispatcher servlets.
  */
 public abstract class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 0;
+
+    private HashMap<String, HashMap<String, LinkedList<Method>>> resources = new HashMap<>();
+
+    private static final String UTF_8_ENCODING = "UTF-8";
+
+    private static final String MULTIPART_FORM_DATA_MIME_TYPE = "multipart/form-data";
+
+    @Override
+    public void init() throws ServletException {
+        // TODO Populate resource map (path/method/handler list)
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Look up handler list
+        String servletPath = request.getServletPath();
+
+        HashMap<String, LinkedList<Method>> resource = resources.get(servletPath);
+
+        if (resource == null) {
+            super.service(request, response);
+            return;
+        }
+
+        LinkedList<Method> handlerList = resource.get(request.getMethod());
+
+        if (handlerList == null) {
+            response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            return;
+        }
+
+        // Set character encoding
+        if (request.getCharacterEncoding() == null) {
+            request.setCharacterEncoding(UTF_8_ENCODING);
+        }
+
+        // Populate parameter map
+        HashMap<String, LinkedList<String>> parameterMap = new HashMap<>();
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+
+        while (parameterNames.hasMoreElements()) {
+            String name = parameterNames.nextElement();
+            String[] values = request.getParameterValues(name);
+
+            LinkedList<String> valueList = new LinkedList<>();
+
+            for (int i = 0; i < values.length; i++) {
+                valueList.add(values[i]);
+            }
+
+            parameterMap.put(name, valueList);
+        }
+
+        // Populate file map
+        HashMap<String, LinkedList<File>> fileMap = new HashMap<>();
+
+        String contentType = request.getContentType();
+
+        if (contentType != null && contentType.startsWith(MULTIPART_FORM_DATA_MIME_TYPE)) {
+            for (Part part : request.getParts()) {
+                String submittedFileName = part.getSubmittedFileName();
+
+                if (submittedFileName == null || submittedFileName.length() == 0) {
+                    continue;
+                }
+
+                String name = part.getName();
+
+                LinkedList<File> fileList = fileMap.get(name);
+
+                if (fileList == null) {
+                    fileList = new LinkedList<>();
+                    fileMap.put(name, fileList);
+                }
+
+                File file = File.createTempFile(part.getName(), "_" + part.getSubmittedFileName());
+                part.write(file.getAbsolutePath());
+
+                fileList.add(file);
+            }
+        }
+
+        // Invoke handler method
+        ServletContext servletContext = getServletContext();
+
+        Class<?> type = getClass();
+        String typeName = type.getName();
+
+        Method method = getMethod(handlerList, parameterMap, fileMap);
+
+        if (method == null) {
+            response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            return;
+        }
+
+        Class<?> returnType = method.getReturnType();
+
+        TemplateEncoder templateEncoder = null;
+
+        if (returnType != Void.TYPE && returnType != Void.class) {
+            Handler[] handlers = method.getAnnotationsByType(Handler.class);
+
+            for (int i = 0; i < handlers.length; i++) {
+                Handler handler = handlers[i];
+
+                if (handler.path().equals(servletPath)) {
+                    URL url = type.getResource(handler.path());
+
+                    if (url == null) {
+                        throw new ServletException("Template not found.");
+                    }
+
+                    templateEncoder = new TemplateEncoder(url, typeName);
+
+                    templateEncoder.getContext().putAll(mapOf(
+                        entry("scheme", request.getScheme()),
+                        entry("serverName", request.getServerName()),
+                        entry("serverPort", request.getServerPort()),
+                        entry("contextPath", request.getContextPath())
+                    ));
+
+                    String mimeType = servletContext.getMimeType(servletPath);
+
+                    if (mimeType != null) {
+                        response.setContentType(String.format("%s;charset=%s", mimeType, handler.charset()));
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        // TODO Set thread-local request and response properties
+
+        Object result = null;
+
+        try {
+            try {
+                result = method.invoke(this, getArguments(method, parameterMap, fileMap));
+            } catch (Exception exception) {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+                Throwable cause = exception.getCause();
+
+                if (cause != null) {
+                    servletContext.log(typeName, cause);
+                }
+
+                return;
+            }
+
+            // Write response
+            if (returnType == Void.TYPE || returnType == Void.class) {
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else {
+                try {
+                    templateEncoder.writeValue(result, response.getOutputStream());
+                } catch (IOException exception) {
+                    servletContext.log(typeName, exception);
+                }
+            }
+        } finally {
+            // Close result
+            if (result instanceof AutoCloseable) {
+                try {
+                    ((AutoCloseable)result).close();
+                } catch (Exception exception) {
+                    servletContext.log(typeName, exception);
+                }
+            }
+
+            // Delete files
+            for (LinkedList<File> fileList : fileMap.values()) {
+                for (File file : fileList) {
+                    file.delete();
+                }
+            }
+        }
+    }
+
+    private static Method getMethod(LinkedList<Method> handlerList, HashMap<String, LinkedList<String>> parameterMap,
+        HashMap<String, LinkedList<File>> fileMap) {
+        Method method = null;
+
+        int n = parameterMap.size() + fileMap.size();
+
+        int i = Integer.MAX_VALUE;
+
+        for (Method handler : handlerList) {
+            Parameter[] parameters = handler.getParameters();
+
+            if (parameters.length >= n) {
+                int j = 0;
+
+                for (int k = 0; k < parameters.length; k++) {
+                    String name = parameters[k].getName();
+
+                    if (!(parameterMap.containsKey(name) || fileMap.containsKey(name))) {
+                        j++;
+                    }
+                }
+
+                if (parameters.length - j == n && j < i) {
+                    method = handler;
+
+                    i = j;
+                }
+            }
+        }
+
+        return method;
+    }
+
+    private static Object[] getArguments(Method method, HashMap<String, LinkedList<String>> parameterMap,
+        HashMap<String, LinkedList<File>> fileMap) throws IOException {
+        Parameter[] parameters = method.getParameters();
+
+        Object[] arguments = new Object[parameters.length];
+
+        for (int i = 0; i < parameters.length; i++) {
+            Parameter parameter = parameters[i];
+
+            String name = parameter.getName();
+            Class<?> type = parameter.getType();
+
+            Object argument;
+            if (type == List.class) {
+                ParameterizedType parameterizedType = (ParameterizedType)parameter.getParameterizedType();
+                Type elementType = parameterizedType.getActualTypeArguments()[0];
+
+                List<Object> list;
+                if (elementType == URL.class) {
+                    LinkedList<File> fileList = fileMap.get(name);
+
+                    if (fileList != null) {
+                        list = new ArrayList<>(fileList.size());
+
+                        for (File file : fileList) {
+                            list.add(file.toURI().toURL());
+                        }
+                    } else {
+                        list = Collections.emptyList();
+                    }
+                } else {
+                    LinkedList<String> valueList = parameterMap.get(name);
+
+                    if (valueList != null) {
+                        int n = valueList.size();
+
+                        list = new ArrayList<>(n);
+
+                        for (String value : valueList) {
+                            list.add(getArgument(value, elementType));
+                        }
+                    } else {
+                        list = Collections.emptyList();
+                    }
+                }
+
+                argument = list;
+            } else if (type == URL.class) {
+                LinkedList<File> fileList = fileMap.get(name);
+
+                if (fileList != null) {
+                    argument = fileList.getFirst().toURI().toURL();
+                } else {
+                    argument = null;
+                }
+            } else {
+                LinkedList<String> valueList = parameterMap.get(name);
+
+                String value;
+                if (valueList != null) {
+                    value = valueList.getFirst();
+                } else {
+                    value = null;
+                }
+
+                argument = getArgument(value, type);
+            }
+
+            arguments[i] = argument;
+        }
+
+        return arguments;
+    }
+
+    private static Object getArgument(String value, Type type) {
+        Object argument;
+        if (type == String.class) {
+            argument = value;
+        } else if (type == Byte.TYPE) {
+            argument = (value == null) ? 0 : Byte.parseByte(value);
+        } else if (type == Byte.class) {
+            argument = (value == null) ? null : Byte.parseByte(value);
+        } else if (type == Short.TYPE) {
+            argument = (value == null) ? 0 : Short.parseShort(value);
+        } else if (type == Short.class) {
+            argument = (value == null) ? null : Short.parseShort(value);
+        } else if (type == Integer.TYPE) {
+            argument = (value == null) ? 0 : Integer.parseInt(value);
+        } else if (type == Integer.class) {
+            argument = (value == null) ? null : Integer.parseInt(value);
+        } else if (type == Long.TYPE) {
+            argument = (value == null) ? 0 : Long.parseLong(value);
+        } else if (type == Long.class) {
+            argument = (value == null) ? null : Long.parseLong(value);
+        } else if (type == Float.TYPE) {
+            argument = (value == null) ? 0 : Float.parseFloat(value);
+        } else if (type == Float.class) {
+            argument = (value == null) ? null : Float.parseFloat(value);
+        } else if (type == Double.TYPE) {
+            argument = (value == null) ? 0 : Double.parseDouble(value);
+        } else if (type == Double.class) {
+            argument = (value == null) ? null : Double.parseDouble(value);
+        } else if (type == Boolean.TYPE) {
+            argument = (value == null) ? false : Boolean.parseBoolean(value);
+        } else if (type == Boolean.class) {
+            argument = (value == null) ? null : Boolean.parseBoolean(value);
+        } else if (type == Date.class) {
+            argument = new Date(Long.parseLong(value));
+        } else if (type == LocalDate.class) {
+            argument = LocalDate.parse(value);
+        } else if (type == LocalTime.class) {
+            argument = LocalTime.parse(value);
+        } else if (type == LocalDateTime.class) {
+            argument = LocalDateTime.parse(value);
+        } else {
+            throw new UnsupportedOperationException("Invalid parameter type.");
+        }
+
+        return argument;
+    }
 
     /**
      * Creates a list from a variable length array of elements.
